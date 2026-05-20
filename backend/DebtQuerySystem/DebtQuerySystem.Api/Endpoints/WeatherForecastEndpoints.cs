2b947cd16952b17f120e9b20a570cb80090440f5
Danilo Alves <daniloalves.tech@gmail.com>
2026-05-19 23:18:22 -0300

    feat(arch): setup clean architecture boilerplate with minimal apis and usecases

---
+++ backend/DebtQuerySystem/DebtQuerySystem.Api/Endpoints/WeatherForecastEndpoints.cs
@@ -0,0 +1,13 @@
+﻿using DebtQuerySystem.Application.WeatherForecast;
+using Microsoft.AspNetCore.Http.HttpResults;
+
+namespace DebtQuerySystem.Api.Endpoints
+{
+    public static class WeatherForecastEndpoints
+    {
+        internal static void MapWeatherForecastEndpoints(this WebApplication app)
+        {
+            app.MapGet("/weatherforecast", GetWeatherForecastUseCase.Action).WithName(nameof(GetWeatherForecastUseCase));
+        }
+    }
+}
